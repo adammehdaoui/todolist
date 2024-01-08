8a2b0acb1a7c9a96f4d0f50b10d910498a1a8420
Adam <adam.mehdaouijorge@gmail.com>
2024-01-08 14:36:19 +0100

    chore: using only div for Checkbox component

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Checkbox({ checked, handleClickOnInput }) {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleClickOnInput();
+    }
+  };
+
   return (
-    <span
+    <div
       role="checkbox"
       aria-checked={checked}
       onClick={handleClickOnInput}
-      onKeyDown={(e) => {
-        if (e.key === "Enter") {
-          handleClickOnInput();
-        }
-      }}
+      onKeyDown={(e) => handleKeyDown(e)}
       tabIndex={0}
     >
       {checked ? (
@@ -19,7 +21,7 @@ function Checkbox({ checked, handleClickOnInput }) {
       ) : (
         <div className="h-4 w-4 mt-1 border-solid border-2 border-gray-300 rounded" />
       )}
-    </span>
+    </div>
   );
 }
 
